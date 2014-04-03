6dac73a2df575432688eff1826454d3639804c20
orkhan <orkhan.maharramli@gmail.com>
2014-04-03 15:19:58 +0500

    Some fix

--- src/config/phrase.php
+++ src/config/phrase.php
@@ -5,7 +5,7 @@ return [
         return true; // Your custom filter: Sentry::check() && Sentry::getUser()->hasAccess('laraphrases') ? true : false
     },
     'is_editable_mode_on' => function() {
-        return $_COOKIE['editing_mode'] === "false" ? false : true;
+        return isset($_COOKIE['editing_mode']) && $_COOKIE['editing_mode'] === "false" ? false : true;
     },
     'white_list' => [
         'Phrase' => ['value'],
